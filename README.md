30721d221742b79cb6e6eb3a345390b8b2ca2410
tbdunckel60 <82952632+tbdunckel60@users.noreply.github.com>
2021-05-07 09:43:37 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,84 @@
+At the beginning you are told you are going to Neptune. You will be thrown off course and have to complete levels and challenges to get to the end. Maybe you will never reach the planet, maybe you will discover a new one, and maybe you will die. Each level is different. There will be choices. With each choice, you will access more levels. One choice could kill you, another could take you to asteroid field, a different choice could take you to a space snake that will eat you unless you answer it's riddles.
+
+A bunch of mini games to get through when you get to the end of a game you move onto another or are faced with a choice, each choice can take you to different levels or kill you.
+
+ Anything could kill you, there will possibly be second chances and opportunities to collect health. There will also be a secret level, if you complete it you will either unlock something or get straight to the planet you want to get to.
+
+You basically are just trying to survive/get to the planet of your choosing. Another thing is at the beginning you will get to choose the colors of your spaceship.
+You can move in all different directions, but this can vary depending on the level(Your movement might be restricted to just side to side)
+Some of the objects in this game are the spaceship, which you are in. There are also asteroids, aliens, and other objects that might kill you
+
+
+
+
+
+
+
+
+[![Work in MakeCode](https://classroom.github.com/assets/work-in-make-code-c53f0c86300af1a64cdd5dc830e2509efd17c8cb483a722cacaee84d10eb8ec9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=4630044&assignment_repo_type=AssignmentRepo)
+ THIS IS YOUR README FILE.  IT needs your updates!  Add your game design doc here :)
+
+
+> Open this page at [https://mrlilholt.github.io/game-starter/](https://mrlilholt.github.io/game-starter/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mrlilholt/game-starter** and import
+
+## Edit this project ![Build status badge](https://github.com/mrlilholt/game-starter/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mrlilholt/game-starter** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/mrlilholt/game-starter/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://tbdunckel60.github.io/spacesurferrealjsversion/](https://tbdunckel60.github.io/spacesurferrealjsversion/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/tbdunckel60/spacesurferrealjsversion** and import
+
+## Edit this project ![Build status badge](https://github.com/tbdunckel60/spacesurferrealjsversion/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/tbdunckel60/spacesurferrealjsversion** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/tbdunckel60/spacesurferrealjsversion/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,135 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002010101010101070101010101010102010601010103010101010101060103010101050101010101010501010101010101010105010103010101010101050101010701010101010101010103010101010101010101050101040101010101030101030101010101010101010101010101010101010101010101010101050101010101010106010105010101010101010101010501010101010107010101010106050101010101040101010101010101010101010101010101010103010501010101010101050101010101010101010501010601010101010101010107010101010105010104010101050101010101040101010101010101010101010601010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8], TileScale.Sixteen);
+            case "level0":
+            case "level2":return tiles.createTilemap(hex`14002d00050505050505050505050505050505050505050503030303030303030303030303030303030303030303030302030303030203030306030303030302030302030303030203030303030303030303030303030303030303030303030203030303060303030303030303030303030303030303030303030203030203030301010206020106010602030303030303030306010203030303030303030303020303020303060103030303030303030303030303030303060101030302030203030303020303030302030303030303030303030303030303030303030303030302030301030303030302030303030203030303030303030603030203030303030303030303020303030203010303030301020102030303030303030203030306020101020603030302010606060606030303030303030303030303030301030303030303030303030303030303030303030603030303030303020101020303030206010201010303060303030303010303030303060303030303030306030302030302030303030302030303030303030101030303030203030302030603010202010303030603030302020203030301010201030303030303010303020303030303030303030303030303030306030203030303030303020303030303030303030106030303030303030303030303030303030303030303030303020106020602060103030301010303030303010106030303010303030303030301010303030301030203030301030303030203030301010303030603030302010203030203030303030301030303020303030303030303020303030302010203030301020602030303030203030102010103030303030303030303030303030303060303030303030303030303030303030303030302030303020103060102060201060606010303030603030201030303030301060303030606030303020303020303030302030303030301010303030303030301030302030303030303010603030303030303030203030303030203030303010103060302030303030303030303030303030303030303030303030303030203030303030303030303030303030303060303030302010103060302010603030106010303030601020303030303030303030303030303030303030303030303030303030203030303030302020303020303030303030303030303030303030303030303030404040404040404040404040404040404040404`, img`
+....................
+....................
+....2....2...2.....2
+..2....2............
+...........2....2...
+..................2.
+.2...2222222222.....
+...222..........2..2
+..22................
+222..2.2....2....2..
+....................
+.2..2.....2....2....
+....2..2..........2.
+..2.2....2222.......
+2...222222...2222222
+..............2.....
+..............2.....
+..2222...222222..2..
+...2.....2.......2..
+2..2.....2.......22.
+...2...2.2.2222...2.
+..222...2222......2.
+.2................2.
+2.......2.........22
+....................
+....22222222...22...
+..222...2.......22..
+..2.2...2....2...22.
+..2...222..2......2.
+..2........2....222.
+..2222....2..2222...
+.............2......
+.............2...22.
+2222222222...2..22..
+...22...22...2..2...
+.2.....22.......2..2
+......22........2...
+..2....22.2.2.......
+..................2.
+...............2....
+222.2.222..222...222
+....................
+.......2......22..2.
+....................
+....................
+`, [myTiles.transparency16,myTiles.tile10,myTiles.tile2,myTiles.tile1,myTiles.tile8,myTiles.tile9,myTiles.tile11], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
